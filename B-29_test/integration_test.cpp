99aeb93bd9a1a3e34857dfee8d83ab85668f58c4
Alfred Lorber <aalorbe@sandia.gov>
2016-02-08 20:54:23 -0700

    integration_test.cpp: Got invoke to work correctly with member functions

--- B-29_test/integration_test.cpp
+++ B-29_test/integration_test.cpp
@@ -31,10 +31,13 @@ uint16_t Integration::getAnalogReadValue(int pin) {
 
   switch (pin) {
   case A0:
+    std::cerr << "Integration::getAnalogReadValue: case A0" << std::endl;
     return pc1Value;
   case A4:
+    std::cerr << "Integration::getAnalogReadValue: case A4" << std::endl;
     return pc2Value;
   case A5:
+    std::cerr << "Integration::getAnalogReadValue: case A5" << std::endl;
     return bcValue;
   default:
     ADD_FAILURE() << "We should never get here.";
@@ -42,7 +45,7 @@ uint16_t Integration::getAnalogReadValue(int pin) {
   }
 }
 
-TEST(Integration, CycleThrougDay) {
+TEST_F(Integration, CycleThroughDay) {
   ArduinoMock * arduinoMock = arduinoMockInstance();
   
   EXPECT_CALL(*arduinoMock, pinMode(AnyOf(O1,O2,O3,O4,O5,O6,O7,O8,O13),OUTPUT))
@@ -54,7 +57,6 @@ TEST(Integration, CycleThrougDay) {
   EXPECT_CALL(*arduinoMock, analogRead(A0))
     .Times(AtLeast(1))
     .WillRepeatedly(Invoke(this,&Integration::getAnalogReadValue));
-    //    .WillRepeatedly(Invoke(getAnalogReadValue));
   
   EXPECT_CALL(*arduinoMock, analogRead(A4))
     .Times(AtLeast(1))
@@ -70,6 +72,10 @@ TEST(Integration, CycleThrougDay) {
     
   Lucky7 lucky7 = Lucky7();
   lucky7.setup();
+
+  analogRead(A4);
+  analogRead(A5);
+  analogRead(A0);
 
   releaseArduinoMock();
   releaseIRrecvMock();
