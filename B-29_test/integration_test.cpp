2ce4e689e5204f43781a7f6e2d3caefb71db782f
Alfred Lorber <aalorbe@sandia.gov>
2016-02-11 19:46:01 -0700

    integration_test.cpp: First working integration test

--- B-29_test/integration_test.cpp
+++ B-29_test/integration_test.cpp
@@ -7,6 +7,7 @@ using ::testing::_;
 using ::testing::AnyOf;
 using ::testing::AtLeast;
 using ::testing::Invoke;
+using ::testing::Return;
 
 void Integration::SetUp() {
   pc1Value = 0;
@@ -28,16 +29,12 @@ void Integration::setAnalogReadValue(int pin, uint16_t pinValue) {
 }
 
 uint16_t Integration::getAnalogReadValue(int pin) {
-
   switch (pin) {
   case A0:
-    std::cerr << "Integration::getAnalogReadValue: case A0" << std::endl;
     return pc1Value;
   case A4:
-    std::cerr << "Integration::getAnalogReadValue: case A4" << std::endl;
     return pc2Value;
   case A5:
-    std::cerr << "Integration::getAnalogReadValue: case A5" << std::endl;
     return bcValue;
   default:
     ADD_FAILURE() << "We should never get here.";
@@ -46,39 +43,94 @@ uint16_t Integration::getAnalogReadValue(int pin) {
 }
 
 TEST_F(Integration, CycleThroughDay) {
+  IRrecvMock * irrecvMock = irrecvMockInstance();
+  EXPECT_CALL(*irrecvMock, enableIRIn())
+    .Times(AtLeast(1));
+  EXPECT_CALL(*irrecvMock, decode(_))
+    .Times(AtLeast(1));
+
+  EXPECT_CALL(*irrecvMock, resume())
+    .Times(AtLeast(1));
+
+  EEPROMMock * eepromMock  = EEPROMMockInstance();
+  EXPECT_CALL(*eepromMock, read(_))
+    .Times(AtLeast(1));
+
+  SerialMock  * serialMock  = serialMockInstance();
+  EXPECT_CALL(*serialMock, begin(_))
+    .Times(1);
+  EXPECT_CALL(*serialMock, available())
+    .WillRepeatedly(Return(0));
+
   ArduinoMock * arduinoMock = arduinoMockInstance();
   
   EXPECT_CALL(*arduinoMock, pinMode(AnyOf(O1,O2,O3,O4,O5,O6,O7,O8,O13),OUTPUT))
     .Times(9);
-  
   EXPECT_CALL(*arduinoMock, pinMode(AnyOf(A0,A4,A5),INPUT))
     .Times(AtLeast(1));
-  
   EXPECT_CALL(*arduinoMock, analogRead(A0))
     .Times(AtLeast(1))
     .WillRepeatedly(Invoke(this,&Integration::getAnalogReadValue));
-  
   EXPECT_CALL(*arduinoMock, analogRead(A4))
     .Times(AtLeast(1))
     .WillRepeatedly(Invoke(this,&Integration::getAnalogReadValue));
-  
   EXPECT_CALL(*arduinoMock, analogRead(A5))
     .Times(AtLeast(1))
     .WillRepeatedly(Invoke(this,&Integration::getAnalogReadValue));
-  
-  IRrecvMock * irrecvMock = irrecvMockInstance();
-  EXPECT_CALL(*irrecvMock, enableIRIn())
-    .Times(1);
-    
-  Lucky7 lucky7 = Lucky7();
-  lucky7.setup();
+  EXPECT_CALL(*arduinoMock, analogWrite(_,_))
+    .Times(AtLeast(1));
+  EXPECT_CALL(*arduinoMock, digitalWrite(_,_))
+    .Times(AtLeast(1));
+  EXPECT_CALL(*arduinoMock, millis())
+    .Times(AtLeast(1));
 
-  analogRead(A4);
-  analogRead(A5);
-  analogRead(A0);
+  Serial.setPrintToCout(true);
 
+  const uint16_t volts12 = uint16_t(12.0/float(BVSCALE) + .5);
+  const uint16_t volts14 = uint16_t(14.0/float(BVSCALE) + .5);
+
+  for (uint8_t j = 0; j < AVECNT; j++) {
+    hw.pc1[j] = 0;
+    hw.pc2[j] = 0;
+    hw.bc[j] = volts12;
+  }
+
+  arduinoMock->setMillisRaw(0);
+
+  setup();
+  for (uint32_t i = 0; i < 3600000; i++) {
+    arduinoMock->addMillisRaw(10);
+    for (uint8_t j = 0; j < AVECNT; j++) {
+      hw.pc1[j] = 0;
+      hw.pc2[j] = 0;
+      hw.bc[j] = volts14;
+    }
+    loop();
+  }
+  for (uint32_t i = 0; i < 2888000; i++) {
+    arduinoMock->addMillisRaw(10);
+    for (uint8_t j = 0; j < AVECNT; j++) {
+      hw.pc1[j] = 1000;
+      hw.pc2[j] = 1000;
+      hw.bc[j] = volts14;
+    }
+    loop();
+  }
+  for (uint32_t i = 0; i < 1444000; i++) {
+    arduinoMock->addMillisRaw(10);
+    for (uint8_t j = 0; j < AVECNT; j++) {
+      hw.pc1[j] = 0;
+      hw.pc2[j] = 0;
+      hw.bc[j] = volts14;
+    }
+    loop();
+  }
+   
   releaseArduinoMock();
+  releaseSerialMock();
   releaseIRrecvMock();
+  releaseEEPROMMock();
+
 }
 
 
