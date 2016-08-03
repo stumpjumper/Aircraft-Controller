8af75f9639e9a1248b640f0b55eb1bd8e8b056e5
Alfred Lorber <Alfred@TheLorbers.com>
2016-08-02 19:47:38 -0600

    Adding B-52a and B-52b code.  Added setDayInit(),... step, and modified tests to accomidate

---
+++ B-29/B-52a/B-52a.ino
@@ -0,0 +1,248 @@
+#include "aircraft.h"
+#include <EEPROM.h>
+
+// There's an interrupt collision with the IR routines and the PWM
+// Outputs 1, 5, 6 can be dimmed
+// Outputs 2, 3, 7 cannont be dimmed
+// Outputs 4, 8 and 13 are digital, cannot be dimmed
+//
+// Output 8 is the red led on the board
+// Output 13 is the blue led on the board
+
+// Output mapping to lights/motors
+// Lights:
+// 
+// Output types: (d)  = dimable, (nd) = not dimable
+//
+// a1~   Taxi: Wings and Landing Gear (d,3)
+// a2    Landing: Gear Doors, Rt Gear (nd,3)
+// a3    Terrain: (nd,1)
+// a4    Terrain Motor: (nd, motor) Not yet used
+// a5~   Navigation: Wing Tip, Vertical Stab, Bomb Bay (d,5)
+// a6~   (Anti-)Collision: (d,6)
+// a7    (Anti-)Collision Motor: (nd, motor) Not yet used
+
+// Light objects to control each channel
+DecayLight      taxi;            // Taxi lights on right-fwd landing gear and lft & rht wing landing gear (3)
+Light           landing;         // Landing lights on rht-fwd landing gear lft & rht fwd gear doors and (3)
+Light           terrain;         // Terrain light just ahead of most fwd landing gear (1)
+// UpDownMotor  terrainMotor;    // Terrain light up-down motor just ahead of most fwd landing gear (1)
+DecayLight      navigation;      // Navigation lights on lft & right wing tips, two on vertical stab and on on rht bomb-bay door (5)
+DecayLight      collision;       // Anti-Collision lights on lft & rht top before vertical stab and bottom under 47 section (6)
+// UpdDownMotor collisionMotor;  // Anti-Collision motor: On/Off motor that rotates Anti-Collision lights (3)
+
+
+// Flashing settings for taxi lights during day and night
+const uint8_t  taxiMaxLightLevelDay    = uint8_t(.8*ON); // 80% Max
+const uint8_t  taxiMaxLightLevelNight  = uint8_t(.6*ON); // 60% Max
+uint32_t taxiDayOnLengths[1]           = {180000}; // On 3 minutes
+uint32_t taxiDayDecayLengths[1]        = { 60000}; // Off for 1 minute
+uint8_t  taxiDayMaxLightLevels[1]      = {taxiMaxLightLevelDay};  
+uint32_t * taxiDayTauInMilliseconds    = NULL;     // On/Off, no decay
+
+// Flashing settings for navigation lights
+uint32_t navigationOnLengths[1]          = {100};  // On for .1 seconds
+uint32_t navigationDecayLengths[1]       = {1100}; // Decay for 1.1
+uint8_t  navigationMaxLightLevels[1]     = {ON};  
+uint32_t navigationTauInMilliseconds[1]  = {100};  // Half-life = .1 seconds
+
+// Flashing settings for collision lights
+uint32_t collisionOnLengths[1]         = {50};   // On : .05s, .05s
+uint32_t collisionDecayLengths[1]      = {1500}; // Off: .25s, then 1.75 s
+uint8_t  collisionMaxLightLevels[1]    = {ON};   // Full power
+uint32_t * collisionTauInMilliseconds  = NULL;   // On/Off, no decay
+
+void serialPrintBanner() {
+    Serial.println(F("NMNSH B-52 Lighting Controller A"));
+}
+
+bool overrideBatteryLow() {
+  //Not used yet.  May be used when we have motors.  See B-29 for sample usage.
+  return false;
+}
+
+void allLightsOn() {
+  taxi        .on();
+  landing     .on();
+  terrain     .on();
+  navigation  .on();
+  collision   .on();
+}
+
+void allLightsOff() {
+  taxi        .off();
+  landing     .off();
+  terrain     .off();
+  navigation  .off();
+  collision   .off();
+}
+
+void updateAll() {
+  taxi        .update();
+  landing     .update();
+  terrain     .update();
+  navigation  .update();
+  collision   .update();
+}
+
+void allOff() {
+  allLightsOff();
+}
+
+// -------------------- Time of Day Settings ----------------
+void setEvening() {
+  taxiDayMaxLightLevels[0] = taxiMaxLightLevelNight;  
+  taxi        .flash();
+  landing     .on();
+  terrain     .off();
+  navigation  .flash();
+  collision   .flash();
+}
+
+void setNight() {
+  allOff();
+}
+
+void setPreDawn() {
+  taxiDayMaxLightLevels[0] = taxiMaxLightLevelNight;  
+  taxi        .flash();
+  landing     .on();
+  terrain     .off();
+  navigation  .flash();
+  collision   .flash();
+}
+
+void setMorning() {
+  taxiDayMaxLightLevels[0] = taxiMaxLightLevelDay;  
+  taxi        .flash();
+  landing     .on();
+  terrain     .off();
+  navigation  .flash();
+  collision   .flash();
+}
+
+void setDay() {
+  taxiDayMaxLightLevels[0] = taxiMaxLightLevelDay;  
+  taxi        .flash();
+  landing     .on();
+  terrain     .off();
+  navigation  .flash();
+  collision   .flash();
+}
+
+void processKey(uint32_t key) {
+  Serial.print(F("key "));
+  Serial.println(key, HEX);
+  switch (key) {
+  case '0':
+  case RC65X_KEY0:
+  case RC65X_KEYDOWN: // Control wheel down
+  case RM_YD065_KEY0:
+  case RM_YD065_KEYDOWN:
+    Serial.print(F("Got remote \"0\"\n"));
+    setToMode(MODE_OVERRIDE);
+    allOff();
+    break;
+  case '1':
+  case RC65X_KEY1:
+  case RM_YD065_KEY1:
+    Serial.print(F("Got remote \"1\"\n"));
+    setToMode(MODE_OVERRIDE);
+    taxi.toggle();
+    break;
+  case '2':
+  case RC65X_KEY2:
+  case RM_YD065_KEY2:
+    Serial.print(F("Got remote \"2\"\n"));
+    setToMode(MODE_OVERRIDE);
+    landing.toggle();
+    break;
+  case '3':
+  case RC65X_KEY3:
+  case RM_YD065_KEY3:
+    Serial.print(F("Got remote \"3\"\n"));
+    setToMode(MODE_OVERRIDE);
+    terrain.toggle();
+    break;
+  // case '4':
+  // case RC65X_KEY4:
+  // case RM_YD065_KEY4:
+  //   Serial.print(F("Got remote \"4\"\n"));
+  //   setToMode(MODE_OVERRIDE);
+  //   something.toggle();
+  //   break;
+  case '5':
+  case RC65X_KEY5:
+  case RM_YD065_KEY5:
+    Serial.print(F("Got remote \"5\"\n"));
+    setToMode(MODE_OVERRIDE);
+    navigation.toggle();
+    break;
+  case '6':
+  case RC65X_KEY6:
+  case RM_YD065_KEY6:
+    Serial.print(F("Got remote \"6\"\n"));
+    setToMode(MODE_OVERRIDE);
+    collision.toggle();
+    break;
+//   case '7':
+//   case RC65X_KEY7:
+//   case RM_YD065_KEY7:
+//     Serial.print(F("Got remote \"7\"\n"));
+//     setToMode(MODE_OVERRIDE);
+//     floods.toggle();
+//     break;
+  case 'B':
+  case RC65X_KEYRED:
+  case RM_YD065_KEYRED:
+    Serial.print(F("Got remote \"B\"\n"));
+    setToMode(MODE_BATTERYLOW);
+    break;
+  case '8':
+  case RC65X_KEY8:
+  case RC65X_KEYUP: // Control wheel up
+  case RM_YD065_KEY8:
+  case RM_YD065_KEYUP:
+    Serial.print(F("Got remote \"8\"\n"));
+    setToMode(MODE_OVERRIDE);
+    allLightsOn();
+    break;
+  case 'P':
+  case RC65X_KEYPLAY:
+  case RC65X_KEYSELECT:
+  case RM_YD065_KEYPLAY:
+  case RM_YD065_KEYOK:
+    Serial.print(F("Got remote \"P\"\n"));
+    const uint16_t lightLevel = hw.photocell2();
+    const TimeOfDay::DayPart dayPart = timeOfDay.updateAverage(lightLevel);
+    setToMode(dayPart);
+    break;
+  }
+}
+
+void serialPrintCustomStatus()
+{
+  char buffer[75];
+  sprintf(buffer,
+          "|1:%1i:%3d|2:%3d|3:%3d|5:%1i:%3d|6:%1i:%3d,r:%3d|b:%3d|",
+          int(taxi.getLightMode()), hw.o1,
+          hw.o2,
+          hw.o3,
+          int(navigation.getLightMode()), hw.o5,
+          int(collision.getLightMode()), hw.o6,
+          hw.o13,hw.o8);
+  Serial.print(buffer);
+}
+
+void setupLightingAndMotorChannels()
+{
+  taxi       .setup(hw.o1, taxiMaxLightLevelDay, 
+                    1, taxiDayOnLengths, taxiDayDecayLengths,
+                    taxiDayMaxLightLevels, taxiDayTauInMilliseconds);
+  landing    .setup(hw.o2, ON);
+  terrain    .setup(hw.o3, ON);
+  navigation .setup(hw.o5, ON, 1, navigationOnLengths, navigationDecayLengths,
+                    navigationMaxLightLevels, navigationTauInMilliseconds);
+  collision  .setup(hw.o6,  ON, 1, collisionOnLengths, collisionDecayLengths,
+                    collisionMaxLightLevels, collisionTauInMilliseconds);
+}
---
+++ B-29/B-52b/B-52b.ino
@@ -0,0 +1,253 @@
+#include "aircraft.h"
+#include <EEPROM.h>
+
+// There's an interrupt collision with the IR routines and the PWM
+// Outputs 1, 5, 6 can be dimmed
+// Outputs 2, 3, 7 cannont be dimmed
+// Outputs 4, 8 and 13 are digital, cannot be dimmed
+//
+// Output 8 is the red led on the board
+// Output 13 is the blue led on the board
+
+// Output mapping to lights/motors
+// Lights:
+// 
+// Output types: (d)  = dimable, (nd) = not dimable
+//
+// b1~ Cat Walk: Bomb Bay and Wheel Wells (d 13) 
+// b2  Interior White: Flight Deck and Tail Gunner (nd, ??)  
+// b3  Interior Red: Flight Deck and Tail Gunner (nd, ??)  
+// b4  Cockpit Floods: Interior Cockpit Floods (nd, 2) 
+// b5~ Loader~: (d, 4) 
+// b6~ Tail Flood: Flood lights on tail flash~ (d, 2) 
+// b7 Future
+
+// Light objects to control each channel
+DecayLight catwalk;       // Bomb Bay and Wheel Well lights, mostly on the catwalk (13)
+Light      interiorWhite; // Interior White: Flight Deck and Tail Gunner (nd, ?
+Light      interiorRed;   // Interior Red: Flight Deck and Tail Gunner (nd, ??)
+Light      cockpitFloods; // Two lights behind the pilots (nd, 2)
+DecayLight loader;        // The four lighs on the loader (d, 4)
+Light      tailFloods;    // Two flood lighs on horizontal that illuminate the tail flash (2)
+
+// Decay settings for catwalk lights during day and night
+const uint8_t  catwalkMaxLightLevelDay   = uint8_t(.8*ON); // 80% Max
+const uint8_t  catwalkMaxLightLevelNight = uint8_t(.6*ON); // 60% Max
+uint32_t catwalkDayOnLengths[1]          = {180000}; // On 3 minutes
+uint32_t catwalkDayDecayLengths[1]       = { 61000}; // Off for 1 minute, 10 seconds to keep out of sync
+uint8_t  catwalkDayMaxLightLevels[1]     = {catwalkMaxLightLevelDay};  
+uint32_t * catwalkDayTauInMilliseconds   = NULL;     // On/Off, no decay
+
+// Flashing settings for loader's lights
+const uint8_t  loaderMaxLightLevelDay    = uint8_t(.8*ON); // 80% Max
+const uint8_t  loaderMaxLightLevelNight  = uint8_t(.6*ON); // 60% Max
+uint32_t loaderDayOnLengths[1]           = {180000}; // On 3 minutes
+uint32_t loaderDayDecayLengths[1]        = { 61500}; // Off for 1 minute, 15 seconds to keep out of sync
+uint8_t  loaderDayMaxLightLevels[1]      = {loaderMaxLightLevelDay};  
+uint32_t * loaderDayTauInMilliseconds    = NULL;     // On/Off, no decay
+
+// Settings for tail floods
+const uint8_t  tailFloodsMaxLightLevel   = uint8_t(0.8*ON); // 80% Max
+
+void serialPrintBanner() {
+    Serial.println(F("NMNSH B-52 Lighting Controller B"));
+}
+
+bool overrideBatteryLow() {
+  //Not used yet.  May be used when we have motors.  See B-29 for sample usage.
+  return false;
+}
+
+void allLightsOn() {
+  catwalk       .on();
+  interiorWhite .on();
+  interiorRed   .on();
+  cockpitFloods .on();
+  loader        .on();
+  tailFloods    .on();
+}
+
+void allLightsOff() {
+  catwalk       .off();
+  interiorWhite .off();
+  interiorRed   .off();
+  cockpitFloods .off();
+  loader        .off();
+  tailFloods    .off();
+}
+
+void updateAll() {
+  catwalk       .update();
+  interiorWhite .update();
+  interiorRed   .update();
+  cockpitFloods .update();
+  loader        .update();
+  tailFloods    .update();
+}
+
+void allOff() {
+  allLightsOff();
+}
+
+// -------------------- Time of Day Settings ----------------
+void setEvening() {
+  catwalkDayMaxLightLevels[0] = catwalkMaxLightLevelNight;  
+  catwalk       .flash();
+  interiorWhite .off();
+  interiorRed   .on();
+  cockpitFloods .off();
+  loaderDayMaxLightLevels[0] = loaderMaxLightLevelNight;
+  loader        .flash();
+  tailFloods    .on();
+}
+
+void setNight() {
+  allOff();
+}
+
+void setPreDawn() {
+  catwalk       .off();
+  interiorWhite .off();
+  interiorRed   .off();
+  cockpitFloods .off();
+  loader        .off();
+  tailFloods    .on();
+}
+
+void setMorning() {
+  catwalk       .off();
+  interiorWhite .off();
+  interiorRed   .off();
+  cockpitFloods .off();
+  loader        .off();
+  tailFloods    .off();
+}
+
+void setDay() {
+  catwalkDayMaxLightLevels[0] = catwalkMaxLightLevelDay;  
+  catwalk       .flash();
+  interiorWhite .off();
+  interiorRed   .off();
+  cockpitFloods .off();
+  loaderDayMaxLightLevels[0] = loaderMaxLightLevelDay;
+  loader        .flash();
+  tailFloods    .off();
+}
+
+void processKey(uint32_t key) {
+  Serial.print(F("key "));
+  Serial.println(key, HEX);
+  switch (key) {
+  case '0':
+  case RC65X_KEY0:
+  case RC65X_KEYDOWN: // Control wheel down
+  case RM_YD065_KEY0:
+  case RM_YD065_KEYDOWN:
+    Serial.print(F("Got remote \"0\"\n"));
+    setToMode(MODE_OVERRIDE);
+    allOff();
+    break;
+  case '1':
+  case RC65X_KEY1:
+  case RM_YD065_KEY1:
+    Serial.print(F("Got remote \"1\"\n"));
+    setToMode(MODE_OVERRIDE);
+    catwalk.toggle();
+    break;
+  case '2':
+  case RC65X_KEY2:
+  case RM_YD065_KEY2:
+    Serial.print(F("Got remote \"2\"\n"));
+    setToMode(MODE_OVERRIDE);
+    interiorWhite.toggle();
+    break;
+  case '3':
+  case RC65X_KEY3:
+  case RM_YD065_KEY3:
+    Serial.print(F("Got remote \"3\"\n"));
+    setToMode(MODE_OVERRIDE);
+    interiorRed.toggle();
+    break;
+  case '4':
+  case RC65X_KEY4:
+  case RM_YD065_KEY4:
+    Serial.print(F("Got remote \"4\"\n"));
+    setToMode(MODE_OVERRIDE);
+    cockpitFloods.toggle();
+    break;
+  case '5':
+  case RC65X_KEY5:
+  case RM_YD065_KEY5:
+    Serial.print(F("Got remote \"5\"\n"));
+    setToMode(MODE_OVERRIDE);
+    loader.toggle();
+    break;
+  case '6':
+  case RC65X_KEY6:
+  case RM_YD065_KEY6:
+    Serial.print(F("Got remote \"6\"\n"));
+    setToMode(MODE_OVERRIDE);
+    tailFloods.toggle();
+    break;
+//   case '7':
+//   case RC65X_KEY7:
+//   case RM_YD065_KEY7:
+//     Serial.print(F("Got remote \"7\"\n"));
+//     setToMode(MODE_OVERRIDE);
+//     floods.toggle();
+//     break;
+  case 'B':
+  case RC65X_KEYRED:
+  case RM_YD065_KEYRED:
+    Serial.print(F("Got remote \"B\"\n"));
+    setToMode(MODE_BATTERYLOW);
+    break;
+  case '8':
+  case RC65X_KEY8:
+  case RC65X_KEYUP: // Control wheel up
+  case RM_YD065_KEY8:
+  case RM_YD065_KEYUP:
+    Serial.print(F("Got remote \"8\"\n"));
+    setToMode(MODE_OVERRIDE);
+    allLightsOn();
+    break;
+  case 'P':
+  case RC65X_KEYPLAY:
+  case RC65X_KEYSELECT:
+  case RM_YD065_KEYPLAY:
+  case RM_YD065_KEYOK:
+    Serial.print(F("Got remote \"P\"\n"));
+    const uint16_t lightLevel = hw.photocell2();
+    const TimeOfDay::DayPart dayPart = timeOfDay.updateAverage(lightLevel);
+    setToMode(dayPart);
+    break;
+  }
+}
+
+void serialPrintCustomStatus()
+{
+  char buffer[75];
+  sprintf(buffer,
+          "|1:%1i:%3d|2:%3d|3:%3d|5:%1i:%3d|6:%1i:%3d,r:%3d|b:%3d|",
+          int(catwalk.getLightMode()), hw.o1,
+          hw.o2,
+          hw.o3,
+          int(loader.getLightMode()), hw.o5,
+          int(tailFloods.getLightMode()), hw.o6,
+          hw.o13,hw.o8);
+  Serial.print(buffer);
+}
+
+void setupLightingAndMotorChannels()
+{
+  catwalk      .setup(hw.o1, catwalkMaxLightLevelDay, 
+                      1, catwalkDayOnLengths, catwalkDayDecayLengths,
+                      catwalkDayMaxLightLevels, catwalkDayTauInMilliseconds);
+  interiorWhite.setup(hw.o2, ON);
+  interiorRed  .setup(hw.o3, ON);
+  cockpitFloods.setup(hw.o4, ON);
+  loader       .setup(hw.o5, loaderMaxLightLevelDay, 
+                      1, loaderDayOnLengths, loaderDayDecayLengths,
+                      loaderDayMaxLightLevels, loaderDayTauInMilliseconds);
+  tailFloods   .setup(hw.o6, tailFloodsMaxLightLevel);
+}
